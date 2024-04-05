566aa7f8edb8dfacebae7c1bd5eb4a53d9f39547
Anik Chandra Das <126132260+anikdas29@users.noreply.github.com>
2024-04-05 12:34:57 +0600

    Update dataset.js

--- dataset.js
+++ dataset.js
@@ -8,16 +8,6 @@
                 "0" : "Found insights into the diversity, characteristics, and relationships of different indoor plant species.",
                 "1" : "This comprehensive overview enhanced our understanding of their natural habitats, and provided valuable information for plant enthusiasts and horticulturists."
             }   
-        },
-        "1" : {
-            "link" : "https://www.kaggle.com/code/ashikchandradas/co2-emission-regression-analysis-and-visualization",
-            "linkText" : "explore now 🔗",
-            "heading" : "CO2 emission: Regression Analysis with Visualization.",
-            "list" : {
-                "0" : "mpact of Engine Size in the Fuel Consumption and CO2 Emission.",
-                "1" : "Predict the CO2 Emission based on Linear Regression model and evaluation of the model.",
-                "2" : "K-means Clustering the level of Fuel Consumption based on different parameters."
-            }   
         }
     }
 }
