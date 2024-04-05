da513e394d53cb4846ceeb8b56a7fd16e1bc2a77
Anik Chandra Das <126132260+anikdas29@users.noreply.github.com>
2024-04-05 10:59:23 +0600

    some changes

--- dataset.js
+++ dataset.js
@@ -1,11 +1,11 @@
 {
-    "project" : [
+    "project" : {
         "link" : "https://www.kaggle.com/code/ashikchandradas/exploring-indoor-plant-diversity/notebook",
         "linkText" : "null",
         "heading" : "Exploring Indoor Plant Diversity: Insights from Data Visualization.",
-        "list" : [
+        "list" : {
             "0" : "Found insights into the diversity, characteristics, and relationships of different indoor plant species.",
             "1" : "This comprehensive overview enhanced our understanding of their natural habitats, and provided valuable information for plant enthusiasts and horticulturists."
-        ]
-    ]
+        }
+    }
 }
