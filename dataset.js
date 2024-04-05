ccdfc18a230a424e01b20d53d5c40dc551ab5b35
Anik Chandra Das <126132260+anikdas29@users.noreply.github.com>
2024-04-05 11:18:19 +0600

    system for multipal project

--- dataset.js
+++ dataset.js
@@ -1,11 +1,13 @@
 {
     "project" : {
-        "link" : "https://www.kaggle.com/code/ashikchandradas/exploring-indoor-plant-diversity/notebook",
-        "linkText" : "null",
-        "heading" : "Exploring Indoor Plant Diversity: Insights from Data Visualization.",
-        "list" : {
-            "0" : "Found insights into the diversity, characteristics, and relationships of different indoor plant species.",
-            "1" : "This comprehensive overview enhanced our understanding of their natural habitats, and provided valuable information for plant enthusiasts and horticulturists."
+        0 : {
+            "link" : "https://www.kaggle.com/code/ashikchandradas/exploring-indoor-plant-diversity/notebook",
+            "linkText" : "null",
+            "heading" : "Exploring Indoor Plant Diversity: Insights from Data Visualization.",
+            "list" : {
+                0 : "Found insights into the diversity, characteristics, and relationships of different indoor plant species.",
+                1 : "This comprehensive overview enhanced our understanding of their natural habitats, and provided valuable information for plant enthusiasts and horticulturists."
+            }   
         }
     }
 }
